9dde5790f4bfd7d24adf9e4128efa7cb0c9f8b51
cool85runner <71359396+cool85runner@users.noreply.github.com>
2021-03-11 09:07:18 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1989,7 +1989,7 @@ class mainCog(commands.Cog):
 				result = wks.acell(basicSetting[16]).value
 
 				embed = discord.Embed(
-						description= '```' + SearchID + ' 님이 받을 다이야는 ' + result + ' 다이야 입니다.```',
+						description= '```' + SearchID + ' 님이 횟수는 ' + result + ' 입니다.```',
 						color=0xff00ff
 						)
 				await ctx.send(embed=embed, tts=False)
