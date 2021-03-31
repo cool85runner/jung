f54d7bec6b92d320ec83c10fecce5560f9305143
cool85runner <71359396+cool85runner@users.noreply.github.com>
2021-03-31 09:08:39 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1408,7 +1408,7 @@ class mainCog(commands.Cog):
 			command_list += ','.join(command[41]) + ' [추첨인원] (대기시간/초) *(메모)\n'    #!인원체크
 			command_list += ','.join(command[35]) + ' [판매금액] (거래소세금)\n'     #!수수료
 			command_list += ','.join(command[36]) + ' [거래소금액] [실거래금액] (거래소세금)\n'     #!페이백
-			command_list += ','.join(command[13]) + ' [아이디]\n'     #!정산
+			command_list += ','.join(command[13]) + ' [아이디]\n'     #!체크
 			command_list += ','.join(command[14]) + ' 또는 ' + ','.join(command[14]) + ' 0000, 00:00\n'     #!보스일괄
 			command_list += ','.join(command[40]) + ' 또는 ' + ','.join(command[40]) + ' 0000, 00:00\n'     #!멍일괄
 			command_list += ','.join(command[15]) + '\n'     #!q
@@ -1988,7 +1988,7 @@ class mainCog(commands.Cog):
 				result = wks.acell(basicSetting[16]).value
 
 				embed = discord.Embed(
-						description= '```' + SearchID + ' 님이 받을 다이야는 ' + result + ' 다이야 입니다.```',
+						description= '```' + SearchID + ' >>>>>>> ' + result + ' <<<<<< 입니다.```',
 						color=0xff00ff
 						)
 				await ctx.send(embed=embed, tts=False)
